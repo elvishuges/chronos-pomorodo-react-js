76c4dfa3b3feea335ec8cb2b69a080bf9ba33950
Elvis HGS <elvishuges@hotmail.com>
2025-12-18 22:02:30 -0300

    feature: add footer,cycles,countdown default button e default input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Logo />
       </Container>
 
       <Container>
-        <section>MENU</section>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="task"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );

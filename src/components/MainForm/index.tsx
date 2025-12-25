06f341b5b86eca61c4a16d7b5f3f22fbd6730322
Elvis HGS <elvishuges@hotmail.com>
2025-12-25 17:23:22 -0300

    feature: exibindo o tempo de cada tarefa e formatando segundos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,11 +4,18 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 import type { TaskModel } from "../../models/TaskModel";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  // ciclos
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCyleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -28,20 +35,20 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCyleType],
+      type: nextCyleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
 
-    setState((prevState: any) => {
+    setState((prevState) => {
       return {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, // Conferir
+        currentCycle: nextCycle,
         secondsRemaining, // Conferir
-        formattedSecondsRemaining: "00:00", // Conferir
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), // Conferir
         tasks: [...prevState.tasks, newTask],
       };
     });

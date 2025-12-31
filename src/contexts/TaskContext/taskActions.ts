85e339a24a29dee4e691ab62b4a09ec102a4d70d
Elvis HGS <elvishuges@hotmail.com>
2025-12-31 14:31:10 -0300

    feature: adicionando web worker e na contagem regressiva

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -5,20 +5,27 @@ export const TaskActionTypes = {
   START_TASK: "START_TASK",
   INTERRUPT_TASK: "INTERRUPT_TASK",
   RESET_STATE: "RESET_STATE",
+  COUNT_DOWN: "COUNT_DOWN",
+  COMPLETE_TASK: "COMPLETE_TASK",
 } as const;
 
-// 2. Crie um tipo baseado nos valores do objeto
 export type TaskActionTypes =
   (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
 
-export type TaskActionsWithPayload = {
-  type: typeof TaskActionTypes.START_TASK; // Note o 'typeof' aqui
-  payload: TaskModel;
-};
+export type TaskActionsWithPayload =
+  | {
+      type: typeof TaskActionTypes.START_TASK; // adicionado typeof aqui
+      payload: TaskModel;
+    }
+  | {
+      type: typeof TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionsWithoutPayload =
   | { type: typeof TaskActionTypes.RESET_STATE }
-  | { type: typeof TaskActionTypes.INTERRUPT_TASK };
+  | { type: typeof TaskActionTypes.INTERRUPT_TASK }
+  | { type: typeof TaskActionTypes.COMPLETE_TASK };
 
 export type TaskActionModel =
   | TaskActionsWithPayload

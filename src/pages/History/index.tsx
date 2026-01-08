74bf990992ed8dff726c6b43cc24ab0cbc4ac2d6
Elvis HGS <elvishuges@hotmail.com>
2026-01-08 11:44:52 -0300

    feature: adicionando dicionario de para traduzir tipo de tarefa para pt-br

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -7,6 +7,7 @@ import { MainTemplate } from "../../templates/MainTemplate";
 import styles from "./styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 export function History() {
   const { state } = useTaskContext();
@@ -42,13 +43,19 @@ export function History() {
 
             <tbody>
               {state.tasks.map((task) => {
+                const taskTypeDictionary = {
+                  workTime: "Foco",
+                  shortBreakTime: "Descanso curto",
+                  longBreakTime: "Descanso longo",
+                };
+
                 return (
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
-                    <td>{task.type}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
